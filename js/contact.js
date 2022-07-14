2f9bb2b6fd30c3d4d3b39cbab4d93fbd6f45c818
dereki16 <39037916+dereki16@users.noreply.github.com>
2022-07-14 10:42:29 -0700

    Updated contact page

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,12 @@
 let userName = document.querySelector("#name");
 var form = document.getElementById("contact-form");
-var tob = document.getElementsByClassName("yenoh");
+var tob = document.getElementById("yenoh");
 var text = document.getElementById("tex");
 
 
 text.style.display = "none";
 form.addEventListener('submit', function(event){
-    if (tob.length > 0) {
+    if (tob.value.length > 0) {
         event.preventDefault();
     }
     else {
