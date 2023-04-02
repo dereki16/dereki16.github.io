8f5b8c8fee079cedb0e2dba614d32a9117b5aa84
dereki16 <39037916+dereki16@users.noreply.github.com>
2023-04-02 02:54:53 -0700

    Update on website before I lost my work.
    Redid layout and flow of site, SPA for the most part, social links and email are stickied, all content on index page, except for games page, resume can be viewed at click of button, animations now included, redid about me page, and projects layout.

---
+++ js/App.jsx
@@ -0,0 +1,32 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import moon from './moon.png';
+
+// const element = <h1>Hello, World!</h1>;
+// ReactDOM.render(element, document.getElementById('root'));
+
+// import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+// function App() {
+//     return (
+//         <div>
+//             <Parallax page={4}>
+//                 <ParallaxLayer>
+//                     <h2>Welcome</h2>
+//                 </ParallaxLayer>
+//                 <ParallaxLayer 
+//                 offset={1} 
+//                 speed={1} 
+//                 style={{
+//                     backgroundImage: `url(${moon}`,
+//                     backgroundSize: `cover`,
+//                 }}>
+//                     <h2>Web dev is fun</h2>
+//                 </ParallaxLayer>
+//             </Parallax>
+
+//         </div>
+//     );
+// }
+
+// export default App;
--- js/main.js
+++ js/main.js
@@ -1,3 +1,78 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
+
+
+  function copyToClipboard() {
+    const textToCopy = document.getElementById("verticalText").textContent;
+    const textarea = document.createElement("textarea");
+    textarea.value = textToCopy;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    alert("Email successfully copied!");
+  }
+
+
+  
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const video1 = document.getElementById("video1");
+    const video2 = document.getElementById("video2");
+    const prevBtn = document.getElementById("prev-btn");
+    const nextBtn = document.getElementById("next-btn");
+  
+    let currentVideo = 1;
+  
+    prevBtn.addEventListener("click", function () {
+      if (currentVideo === 1) {
+        video1.style.display = "none";
+        video2.style.display = "block";
+        currentVideo = 2;
+      } else {
+        video2.style.display = "none";
+        video1.style.display = "block";
+        currentVideo = 1;
+      }
+    });
+  
+    nextBtn.addEventListener("click", function () {
+      if (currentVideo === 1) {
+        video1.style.display = "none";
+        video2.style.display = "block";
+        currentVideo = 2;
+      } else {
+        video2.style.display = "none";
+        video1.style.display = "block";
+        currentVideo = 1;
+      }
+    });
+  });
+  
+//   import React from 'react';
+
+// function MyComponent() {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <p>This is my React component.</p>
+//     </div>
+//   );
+// }
+
+// export default MyComponent;
+
+
+
 // Originally used for screenshots
 // var slideIndex = [1,3];
 // var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
