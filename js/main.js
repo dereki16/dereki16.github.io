c664bebf728787bc6d450cf60de6397fbd96dc6e
dereki16 <39037916+dereki16@users.noreply.github.com>
2023-04-02 10:24:09 -0700

    Finishing touches
    Changing img formats, finalizing navbars, social links, design choices.

--- js/main.js
+++ js/main.js
@@ -57,41 +57,8 @@ const observer = new IntersectionObserver((entries) => {
       }
     });
   });
-  
-//   import React from 'react';
-
-// function MyComponent() {
-//   return (
-//     <div>
-//       <h1>Hello, world!</h1>
-//       <p>This is my React component.</p>
-//     </div>
-//   );
-// }
-
-// export default MyComponent;
 
 
-
-// Originally used for screenshots
-// var slideIndex = [1,3];
-// var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
-// showDivs(1, 0);
-// showDivs(1, 1);
-// showDivs(1, 2);
-// showDivs(1, 3);
-
-// function plusDivs(n, no) {
-//     showDivs(slideIndex[no] += n, no);
-// }
-
-// function showDivs(n, no) {
-//     var i;
-//     var x = document.getElementsByClassName(slideId[no]);
-//     if (n > x.length) {slideIndex[no] = 1}
-//     if (n < 2) {slideIndex[no] = x.length}
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";  
-//     }
-//     x[slideIndex[no]-1].style.display = "block";  
-// }
+  function closeDropdown() {
+    document.getElementById('nav-checkbox').checked = false;
+  }
