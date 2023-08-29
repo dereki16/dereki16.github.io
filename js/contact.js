8cb7d71dd059c87c77dce325d7d2fefff2773f4d
dereki16 <39037916+dereki16@users.noreply.github.com>
2023-08-29 00:08:50 -0700

    Modified the navbar. Edited the widths of project cards. Fixed font sizing of main section. Fixed social bar. Switched out blur for opacity to speed up animations.

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,3 @@
-let userName = document.querySelector("#name");
-var form = document.getElementById("contact-form");
-var tob = document.getElementById("yenoh");
-var text = document.getElementById("tex");
-
-form.addEventListener('submit', function(event){
-    if (tob.value.length > 0) {
-        event.preventDefault();
-    }
-    else {
-        document.getElementById("contact-form").submit();
-    }
-})
 
 document.querySelectorAll('.myTextarea').forEach(input => {
     input.addEventListener('input', function () {
--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,11 @@
-window.onscroll = function() {myFunction()};
-var navbar = document.getElementById("topnav");
-var sticky = navbar.offsetTop;
+// window.onscroll = function() {myFunction()};
+// var navbar = document.getElementById("topnav");
+// var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
