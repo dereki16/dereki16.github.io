670e16f8e6fc3c7316447bc968204a51f32ec263
dereki16 <39037916+dereki16@users.noreply.github.com>
2025-12-05 01:06:29 -0800

    Revert "Trying something."
    This reverts commit e1723b54aab990f19d632252ab564ef76221ad1f.

--- js/videos.js
+++ js/videos.js
@@ -1,28 +1,28 @@
-// (function() {
-//     var youtube = document.querySelectorAll(".lazy-youtube");
+(function() {
+    var youtube = document.querySelectorAll(".lazy-youtube");
 
-//     for (var i = 0; i < youtube.length; i++) {
-//         // Using an immediately-invoked function expression (IIFE) to correctly pass the index 'i'
-//         (function(index) {
-//             var source = "https://img.youtube.com/vi/" + youtube[index].dataset.embed + "/sddefault.jpg";
+    for (var i = 0; i < youtube.length; i++) {
+        // Using an immediately-invoked function expression (IIFE) to correctly pass the index 'i'
+        (function(index) {
+            var source = "https://img.youtube.com/vi/" + youtube[index].dataset.embed + "/sddefault.jpg";
 
-//             var image = new Image();
-//             image.src = source;
-//             image.setAttribute("alt", "Clickable image that will start video.");
-//             image.addEventListener("load", function() {
-//                 youtube[index].appendChild(this); // 'this' refers to the image itself
-//             });
+            var image = new Image();
+            image.src = source;
+            image.setAttribute("alt", "Clickable image that will start video.");
+            image.addEventListener("load", function() {
+                youtube[index].appendChild(this); // 'this' refers to the image itself
+            });
 
-//             youtube[index].addEventListener("click", function() {
-//                 var iframe = document.createElement("iframe");
-//                 iframe.setAttribute("allow", "autoplay");
-//                 iframe.setAttribute("frameborder", "0");
-//                 iframe.setAttribute("allowfullscreen", "");
-//                 iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
-//                 this.innerHTML = "";
-//                 this.appendChild(iframe);
-//             });
+            youtube[index].addEventListener("click", function() {
+                var iframe = document.createElement("iframe");
+                iframe.setAttribute("allow", "autoplay");
+                iframe.setAttribute("frameborder", "0");
+                iframe.setAttribute("allowfullscreen", "");
+                iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
+                this.innerHTML = "";
+                this.appendChild(iframe);
+            });
 
-//         })(i); // Pass the current index 'i' to the IIFE
-//     }
-// })();
+        })(i); // Pass the current index 'i' to the IIFE
+    }
+})();
